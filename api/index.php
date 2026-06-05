9fb4f4be647bc5ff59a2944f848ce811638b04ba
EmmanuelNsabua <24mn261si@udbl.ac.cd>
2026-06-05 13:04:37 +0200

    Add debug statements

--- api/index.php
+++ api/index.php
@@ -1,4 +1,5 @@
 <?php
+echo "DEBUG: api/index.php is reached.<br>";
 
 // Forward the request to the Laravel public/index.php
 require __DIR__ . '/../public/index.php';
--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+echo "DEBUG: public/index.php is reached.<br>";
+
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
